da012e10bf0024ba2440c95a720f9b98eb2ebe38
Joe Burson <jrburson@gmail.com>
2015-05-23 17:05:58 -0500

    Updated the scale clock to scale all eight notes. I think this should work.

--- controller.vhd
+++ controller.vhd
@@ -16,7 +16,14 @@ architecture Behavioral of controller is
     port (
       clk_50Mhz : in  std_logic;
       rst       : in  std_logic;
-      clk_2Hz   : out std_logic);
+      clk_c4   : out std_logic;
+      clk_d4   : out std_logic;
+      clk_e4   : out std_logic;
+      clk_f4   : out std_logic;
+      clk_g4   : out std_logic;
+      clk_a4   : out std_logic;
+      clk_b4   : out std_logic;    
+      clk_c5   : out std_logic;
   end component scale_clock;
 
   signal c4_osc : std_logic := '0';
@@ -24,16 +31,16 @@ architecture Behavioral of controller is
   signal e4_osc : std_logic := '0';
   signal f4_osc : std_logic := '0';
   signal g4_osc : std_logic := '0';
-  signal a5_osc : std_logic := '0';
-  signal b5_osc : std_logic := '0';
+  signal a4_osc : std_logic := '0';
+  signal b4_osc : std_logic := '0';
   signal c5_osc : std_logic := '0';
   signal c4_out : std_logic := '0';
   signal d4_out : std_logic := '0';
   signal e4_out : std_logic := '0';
   signal f4_out : std_logic := '0';
   signal g4_out : std_logic := '0';
-  signal a5_out : std_logic := '0';
-  signal b5_out : std_logic := '0';
+  signal a4_out : std_logic := '0';
+  signal b4_out : std_logic := '0';
   signal c5_out : std_logic := '0';
 
   signal c4_out2 : std_logic_vector(0 to 7);
@@ -41,8 +48,8 @@ architecture Behavioral of controller is
   signal e4_out2 : std_logic_vector(0 to 7);
   signal f4_out2 : std_logic_vector(0 to 7);
   signal g4_out2 : std_logic_vector(0 to 7);
-  signal a5_out2 : std_logic_vector(0 to 7);
-  signal b5_out2 : std_logic_vector(0 to 7);
+  signal a4_out2 : std_logic_vector(0 to 7);
+  signal b4_out2 : std_logic_vector(0 to 7);
   signal c5_out2 : std_logic_vector(0 to 7);
 
   signal reseter : std_logic := '0';
@@ -53,15 +60,16 @@ begin
     port map (
       clk_50Mhz => clck,
       rst       => reseter,
-      clk_c4    => c4_out);
+      clk_c4    => c4_out); --is this right? Don't we want this to
+                            --be 'clk_c4 => c4_osc'?
 
   c4_out <= c4_osc and notes(0);
   d4_out <= d4_osc and notes(1);
   e4_out <= e4_osc and notes(2);
   f4_out <= f4_osc and notes(3);
   g4_out <= g4_osc and notes(4);
-  a5_out <= a5_osc and notes(5);
-  b5_out <= b5_osc and notes(6);
+  a4_out <= a4_osc and notes(5);
+  b4_out <= b4_osc and notes(6);
   c5_out <= c5_osc and notes(7);
 
   c4_out2 <= '00000000' when not(c4_out) else '11111111';
@@ -69,11 +77,11 @@ begin
   e4_out2 <= '00000000' when not(e4_out) else '11111111';
   f4_out2 <= '00000000' when not(f4_out) else '11111111';
   g4_out2 <= '00000000' when not(g4_out) else '11111111';
-  a5_out2 <= '00000000' when not(a5_out) else '11111111';
-  b5_out2 <= '00000000' when not(b5_out) else '11111111';
+  a4_out2 <= '00000000' when not(a4_out) else '11111111';
+  b4_out2 <= '00000000' when not(b4_out) else '11111111';
   c5_out2 <= '00000000' when not(c5_out) else '11111111';
 
-  dac <= std_logic_vector(unsigned(c4_out) + unsigned(d4_out) + unsigned(e4_out) + unsigned(f4_out) + unsigned(g4_out) + unsigned(a5_out) + unsigned(b5_out) + unsigned(c5_out));
+  dac <= std_logic_vector(unsigned(c4_out) + unsigned(d4_out) + unsigned(e4_out) + unsigned(f4_out) + unsigned(g4_out) + unsigned(a4_out) + unsigned(b4_out) + unsigned(c5_out));
 
   
 end Behavioral;
--- scale_clock.vhd
+++ scale_clock.vhd
@@ -6,30 +6,105 @@ entity scale_clock is
   port (
     clk_50Mhz : in  std_logic;
     rst       : in  std_logic;
-    clk_2Hz   : out std_logic);
+    clk_c4   : out std_logic;
+    clk_d4   : out std_logic;
+    clk_e4   : out std_logic;
+    clk_f4   : out std_logic;
+    clk_g4   : out std_logic;
+    clk_a4   : out std_logic;
+    clk_b4   : out std_logic;    
+    clk_c5   : out std_logic;
 end scale_clock;
 
 architecture Behavioral of scale_clock is
 
-  signal prescaler : unsigned(23 downto 0);
-  signal clk_2Hz_i : std_logic;
+  signal prescaler_c4 : unsigned(23 downto 0);
+  signal prescaler_d4 : unsigned(23 downto 0);
+  signal prescaler_e4 : unsigned(23 downto 0);
+  signal prescaler_f4 : unsigned(23 downto 0);
+  signal prescaler_g4 : unsigned(23 downto 0);
+  signal prescaler_a4 : unsigned(23 downto 0);
+  signal prescaler_b4 : unsigned(23 downto 0);
+  signal prescaler_c5 : unsigned(23 downto 0);
+  signal clk_c4_262Hz : std_logic;
+  signal clk_d4_294Hz : std_logic;
+  signal clk_e4_330Hz : std_logic;
+  signal clk_f4_349Hz : std_logic;
+  signal clk_g4_392Hz : std_logic;
+  signal clk_a4_440Hz : std_logic;
+  signal clk_b4_494Hz : std_logic;
+  signal clk_c5_523Hz : std_logic;
+
 begin
 
   gen_clk : process (clk_50Mhz, rst)
   begin  -- process gen_clk
     if rst = '1' then
-      clk_2Hz_i   <= '0';
-      prescaler   <= (others => '0');
+      clk_c4_262Hz <= '0';
+      clk_d4_294Hz <= '0';
+      clk_e4_330Hz <= '0';
+      clk_f4_349Hz <= '0';
+      clk_g4_392Hz <= '0';
+      clk_a4_440Hz <= '0';
+      clk_b4_494Hz <= '0';
+      clk_c5_523Hz <= '0';
+      prescaler_c4 <= (others => '0');
+      prescaler_d4 <= (others => '0');
+      prescaler_e4 <= (others => '0');
+      prescaler_f4 <= (others => '0');
+      prescaler_g4 <= (others => '0');
+      prescaler_a4 <= (others => '0');
+      prescaler_b4 <= (others => '0');
+      prescaler_c5 <= (others => '0');
     elsif rising_edge(clk_50Mhz) then   -- rising clock edge
-      if prescaler = X"2E978" then     -- 12 500 000 in hex
-        prescaler   <= (others => '0');
-        clk_2Hz_i   <= not clk_2Hz_i;
-      else
-        prescaler <= prescaler + "1";
+      if prescaler_c4 = X"174BC" then     -- 95420 in hex
+        prescaler_c4 <= (others => '0');
+        clk_c4_262Hz <= not clk_c4_262Hz;
+        prescaler_c4 <= prescaler_c4 - 1;
+      elsif prescalar_d4 = X"14C2A" then
+        prescaler_d4 <= (others => '0');
+        clk_d4_294Hz <= not clk_d4_294Hz;
+        prescaler_d4 <= prescaler_d4 - 1;
+      elsif prescalar_e4 = X"127ED" then
+        prescaler_e4 <= (others => '0');
+        clk_e4_330Hz <= not clk_e4_330Hz;
+        prescaler_e4 <= prescaler_e4 - 1;
+      elsif prescalar_f4 = X"117D1" then
+        prescaler_f4 <= (others => '0');
+        clk_f4_349Hz <= not clk_f4_349Hz;
+        prescaler_f4 <= prescaler_f4 - 1;
+      elsif prescalar_g4 = X"F91F" then
+        prescaler_g4 <= (others => '0');
+        clk_g4_392Hz <= not clk_g4_392Hz;
+        prescaler_g4 <= prescaler_g4 - 1;
+      elsif prescalar_a4 = X"DDF2" then
+        prescaler_a4 <= (others => '0');
+        clk_a4_440Hz <= not clk_a4_440Hz;
+        prescaler_a4 <= prescaler_a4 - 1;
+      elsif prescalar_b4 = X"14C2A" then
+        prescaler_b4 <= (others => '0');
+        clk_b4_494Hz <= not clk_b4_494Hz;
+        prescaler_b4 <= prescaler_b4 - 1;
       end if;
+
+      prescaler_c4 <= prescalar_c4 -1;
+      prescaler_d4 <= prescalar_d4 -1;
+      prescaler_e4 <= prescalar_e4 -1;
+      prescaler_f4 <= prescalar_f4 -1;
+      prescaler_g4 <= prescalar_g4 -1;
+      prescaler_a4 <= prescalar_a4 -1;
+      prescaler_b4 <= prescalar_b4 -1;
+      prescaler_c5 <= prescalar_c5 -1;
     end if;
   end process gen_clk;
 
-clk_2Hz <= clk_2Hz_i;
+clk_c4 <= clk_c4_262Hz;
+clk_d4 <= clk_d4_294Hz;
+clk_e4 <= clk_e4_330Hz;
+clk_f4 <= clk_f4_349Hz;
+clk_g4 <= clk_g4_392Hz;
+clk_a4 <= clk_a4_440Hz;
+clk_b4 <= clk_b4_494Hz;
+clk_c5 <= clk_c5_523Hz;
 
 end Behavioral;
